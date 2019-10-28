2ef4618633ee4eb7d3570ed33cbf520c63ce9c17
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-28 16:08:08 -0700

    set up basic express server

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // implement your API here
+const express = require("express");
+
+const server = express();
+
+server.use(express.json());
+
+const port = 5000;
+server.listen(port, () => console.log("\nListening on port 5000...\n"));
