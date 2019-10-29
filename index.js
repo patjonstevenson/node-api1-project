c80eb5e56a00b0b6a8e1e6dd0a61544c497c0014
Patrick Stevenson <pat.jon.stevenson@gmail.com>
2019-10-28 17:50:50 -0700

    completed MVP: completed handling of put request

--- index.js
+++ index.js
@@ -96,5 +96,20 @@ server.delete("/api/users/:id", (req, res) => {
 });
 
 server.put("/api/users/:id", (req, res) => {
-
+    const id = req.params.id;
+    const body = req.body;
+    if (!(body.name || body.bio)) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+    } else {
+        db
+            .update(id, body)
+            .then(num => {
+                if (num === 0) {
+                    res.status(404).json({ message: "The user with the specified ID does not exist." });
+                } else {
+                    res.status(200).json(body);
+                }
+            })
+            .catch();
+    }
 });
